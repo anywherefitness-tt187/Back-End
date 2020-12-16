ed722bc08b1cf9991de384f1be0347be096a554a
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-12-16 02:07:42 -0500

    host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const server = require("./api/server");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`\ *** Server is running on port: ${port} *** \n`);
