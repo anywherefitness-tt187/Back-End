d11c7a93034444a38afded80621cdca07a44cb67
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-12-16 00:20:40 -0500

    login and register set up with jsonwebtoken

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const pgConnection =
+  process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/anywherefitness.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+};
