f6e24f8d3dd9298501fdc4c1144de7183b0ca23b
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-12-16 18:59:28 -0500

    cors and helmet added

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 const server = express();
 
 const usersRouter = require("../users/users-router");
 const authRouter = require("../auth/auth-router");
 
+server.use(helmet());
 server.use(express.json());
+server.user(cors());
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
