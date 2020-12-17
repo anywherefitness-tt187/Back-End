371a3bcbb9bd886758174d0ddb7d594ece952baf
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-12-16 20:11:33 -0500

    fixed production

--- knexfile.js
+++ knexfile.js
@@ -24,11 +24,15 @@ module.exports = {
   },
 
   production: {
-    client: "pg",
-    connection: pgConnection,
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/anywherefitness.db3",
+    },
     pool: {
-      min: 2,
-      max: 10,
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
     },
     migrations: {
       directory: "./data/migrations",
