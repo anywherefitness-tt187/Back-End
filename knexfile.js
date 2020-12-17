926fdc948af0894df0e038b8ac4b336fe78ddf47
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-12-16 19:23:38 -0500

    Jonathan-Portillo

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
-const pgConnection =
-  process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+// const pgConnection =
+//   process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
 
 module.exports = {
   development: {
