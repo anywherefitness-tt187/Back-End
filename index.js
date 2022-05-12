067343c4cda7e6b9cb41daab0a33fe6baedc327f
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2022-05-12 00:04:48 -0400

    small change

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ require("dotenv").config();
 const port = process.env.PORT;
 
 server.listen(port, () => {
-  console.log(`\ ** Server is running on port: ${port} ** \n`);
+  console.log(`\ * Server is running on port: ${port} ** \n`);
 });
